3d184efe72fb2733e6c3b348060df8d73f378f89
lizafortozo <lizafortozo@gmail.com>
2021-02-19 11:47:37 -0500

    first commit

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,12 +1,74 @@
-// const router = require("express").Router();
-// const User = require("../db").import("../models/user");
+const router = require("express").Router();
+const {User} = require("../models");
 
-// router.post("/create", (req, res) => {
-//   User.create({
-//     email: req.body.user.email,
-//     password: req.body.user.password,
-//     firstname: req.body.user.firstname,
-//     lastname: req.body.user.lastname,
-//   }).then(res.send("this is our user/create endpoint"));
-// });
-// module.exports = router;
+const bcrypt = require("bcryptjs");
+const jwt = require ("jsonwebtoken");
+const {UniqueConstraintError} = require ("sequelize/lib/errors");
+
+
+/* SIGN UP*/
+router.post("/signup", async (req, res) => {
+    let {firstname, lastname, username, email, password} = req.body;
+    console.log(req.body);
+
+    try {
+        const newUser = await User.create({
+          firstname,
+          lastname,
+          username,
+          email,
+          password: bcrypt.hashSync(password, 13),
+        });
+        res.status(201).json({
+          message: "User Created!",
+          user: newUser,
+        });
+      } catch (error) {
+        if (error instanceof UniqueConstraintError) {
+          res.status(409).json({
+            message: "Email already in use.",
+          });
+        } else {
+          res.status(500).json({
+            error: "Failed to create user.",
+          });
+        }
+      }
+});
+
+/*LOGIN*/
+router.post("/login", async (req, res) => {
+    let { username, password } = req.body;
+    console.log(req.body);
+  
+    try {
+      let loginUser = await User.findOne({
+        where: { username },
+      });
+  
+      if (loginUser && (await bcrypt.compare(password, loginUser.password))) {
+        const token = jwt.sign(
+          { id: loginUser.id, username: loginUser.username },
+          process.env.JWT_SECRET,
+          { expiresIn: 60 * 60 * 24 }
+        );
+  
+        res.status(200).json({
+          message: "Login Successful!",
+          user: loginUser,
+          sessionToken: token,
+        });
+      } else {
+        res.status(401).json({
+          message: "Login Failed: User information incorrect.",
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        error: "Error logging in!",
+      });
+    }
+  });
+  
+
+module.exports = router;
--- models/user.js
+++ models/user.js
@@ -1,24 +1,32 @@
-// // const sequelize = require("../db");
+const {DataTypes} = require('sequelize');
+const db = require('../db');
 
-// module.exports = (sequelize, DataTypes) => {
-//   const User = sequelize.define("user", {
-//     username: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//       unique: true,
-//     },
-//     password: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     firstname: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     lastname: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//   });
-//   return User;
-// };
+
+
+  const User = db.define("user", {
+    firstname: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lastname: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+     }
+   
+  });
+module.exports = User;
+

1c2910f51f486e61f82573718e19194e43c6851d
Gordon Mason <gmason0702@gmail.com>
2021-03-06 09:52:01 -0500

    fixing table props

--- app.js
+++ app.js
@@ -6,15 +6,20 @@ const sequelize = require("./db");
 let favorite = require("./controllers/favoritecontroller");
 let user = require("./controllers/usercontroller");
 
-
 sequelize.sync();
 app.use(require("./middleware/headers"));
+
+app.options("*", (req, res) => {
+  res.json({
+    status: "OK",
+  });
+});
 
 app.use(express.json());
 
 app.use("/user", user);
 
-const validateSession = require("../middleware/validate-session");
+// const validateSession = require("../middleware/validate-session");
 
 app.use("/favorite", favorite);
 
--- models/favorite.js
+++ models/favorite.js
@@ -1,17 +1,42 @@
 module.exports = (sequelize, DataTypes) => {
-    const Favorite = sequelize.define("favorite", {
-      review: {
-        type: DataTypes.STRING,
-        allowNull: true,
-      },
-      rating: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-      },
-      owner: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-    });
-    return Favorite;
-  };
+  const Favorite = sequelize.define("favorite", {
+    review: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    personalRating: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    owner: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    movieId: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    movieTitle: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    overview: {
+      type: DataTypes.STRING(10000),
+      allowNull: true,
+    },
+
+    posterPath: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    releaseDate: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    vote_average: {
+      type: DataTypes.NUMERIC(2, 1),
+      allowNull: true,
+    },
+  });
+  return Favorite;
+};
